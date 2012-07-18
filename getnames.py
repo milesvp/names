a577332902633b54a79062b2fa7d09f477db48ae
milesvp <milesvp@83c5f06f-114d-0410-a33c-9ed400b1a7cd>
2012-07-18 09:13:09 +0000

    didn't mean to import into root
    git-svn-id: https://svn.picturecrossword.com/names/trunk@181 83c5f06f-114d-0410-a33c-9ed400b1a7cd

---
+++ getnames.py
@@ -0,0 +1,38 @@
+import urllib, urllib2
+from BeautifulSoup import BeautifulSoup as BS
+import pickle
+
+def getHTML(year = 1880):
+    url = "http://www.ssa.gov/cgi-bin/popularnames.cgi"
+    values = {'top': 1000, 'number': 'n', 'yob': str(year)}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url,data)
+    response = urllib2.urlopen(req)
+    html = response.read()
+    return html
+
+def getNames(year = 1880):
+    html = getHTML(year)
+    soup = BS(''.join(html))
+    soup.prettify()
+    tables = soup.findAll('table')
+
+    rows = tables[2].findAll('tr')
+    rowdata = []
+    for tr in rows:
+        cols = tr.findAll('td')
+        row = []
+        for td in cols:
+            row.append(''.join(td.find(text=True)))
+        rowdata.append(row)
+    return rowdata[1:1001]
+
+def pickleNames(year_start = 2011, year_end = 1880):
+    for year in xrange(year_start, year_end-1, -1):
+        print year
+        data = getNames(year)
+        f = open ('names.'+str(year), 'w')
+        pickle.dump(data,f)
+        f.close()
+
+
