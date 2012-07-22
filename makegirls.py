e331239d5cccc474e0663413f9b436b7ca45a555
milesvp <milesvp@83c5f06f-114d-0410-a33c-9ed400b1a7cd>
2012-07-22 06:46:12 +0000

    fixed a bug with strings as ints. added some plotting functions
    git-svn-id: https://svn.picturecrossword.com/names/trunk@187 83c5f06f-114d-0410-a33c-9ed400b1a7cd

--- makegirls.py
+++ makegirls.py
@@ -1,4 +1,9 @@
 import pickle
+import locale
+import pylab
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
 
 girlname_dict_filename = 'girlname.dict'
 
@@ -37,22 +42,49 @@ def makeGirlList():
     for year in girlname_dict.keys():
         for name,count in girlname_dict[year].iteritems():
             try:
-                names[name].append((year,count))
+                names[name][year] = locale.atoi(count)
             except:
-                names[name] = [(year,count)]
+                names[name] = {}
+                names[name][year] = locale.atoi(count)
     return names
 
 def getMinYear(name):
-    return min([year for year, count in name])
+    return min([year for year, count in name.iteritems()])
 
 def getMaxYear(name):
-    return max([year for year, count in name])
+    return max([year for year, count in name.iteritems()])
 
-def getYearRange(names, girlname_dict):
+def getYearRange(names):
     min_years = []
     max_years = []
     for name in names:
-        min_years.append(getMinYear(girlname_dict[name]))
-        max_years.append(getMaxYear(girlname_dict[name]))
+        min_years.append(getMinYear(name))
+        max_years.append(getMaxYear(name))
     return min(min_years), max(max_years)
+
+def getAnnualCounts(name, start_year = None, end_year = None):
+    if not start_year:
+        start_year = getMinYear(name)
+    if not end_year:
+        end_year = getMaxYear(name)
+    counts = []
+    for year in xrange(start_year, end_year+1):
+        if name.has_key(year):
+            counts.append(int(name[year]))
+        else:
+            counts.append(0)
+    return counts
+
+        
+def plot(names):
+    min_year, max_year = getYearRange(names)
+    years = range(min_year, max_year+1)
+    for name in names:
+        pylab.plot(years, getAnnualCounts(name, min_year, max_year))
+    pylab.show()
+
+
+
+
+
 
